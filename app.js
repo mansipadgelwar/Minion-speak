e27198b3942c9d628a672a410ea3153640ca1b57
mansipadgelwar <mpadgelwar@gmail.com>
2021-06-09 14:53:11 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+// var userName = prompt("Enter your username");
+// alert("username " + userName);
+
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-Input");
+var outputDiv = document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslation(text){
+   return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error){
+   console.log("error occured", error);
+   alert("Something wrong with the server!!");
+}
+
+function clickHandler(){
+   var inputTxt = txtInput.value;
+   fetch(getTranslation(inputTxt))
+    .then(response => response.json())
+    .then(json => {
+       var translatedText = json.contents.translated;
+       outputDiv.innerText = translatedText;
+        })
+    .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler);
